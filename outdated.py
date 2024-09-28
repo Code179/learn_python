d8bcb72bcc97e80101fae091803cb2e83ecbccff
Code179 <futureentrepreneur3@gmail.com>
2024-09-28 09:42:04 +0200

    added while loop and error handling

--- outdated.py
+++ outdated.py
@@ -1,26 +1,33 @@
 from data import months
 
 def main():
-    # User input for date 
-    date = input("Date: ")
-    # Convert date format into YYYY/MM/DD
-    date_formatted = convert(date)
-    # Show date in correct format
-    print(date_formatted)
+    while True:
+        # User input for date 
+        date = input("Date: ")
+        # Convert date format into YYYY/MM/DD
+        date_formatted = convert(date)
+        if date_formatted:
+            # Show date in correct format
+            print(date_formatted)
+            break
 
 def convert(date):
     if date[0].isalpha():
-        first, year = date.split(",")
-        month, day = first.split(" ")
-        if month in months:
-            index = months.index(month)
-            month = index + 1
-            return f"{year}/{month}/{day}"
+        try: 
+            first, year = date.split(",")
+            month, day = first.split(" ")
+        except ValueError:
+            pass
+        else: 
+            if month in months:
+                index = months.index(month)
+                month = index + 1
+                return f"{year}/{month:02}/{day:02}"
     else:   
         # Split up string into 3 variables
         month, day, year = date.split("/")
         # Return new date with correct format
-        return f"{year}/{month}/{day}"
+        return f"{year}/{month: 02}/{day: 02}"
 
 if __name__ == "__main__":
     main()
