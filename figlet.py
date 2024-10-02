afb7a029f937ae458969af23781b56f729117511
Code179 <53229265+Code179@users.noreply.github.com>
2024-10-02 21:00:16 +0000

    Finished figlet exercise

--- figlet.py
+++ figlet.py
@@ -1,5 +1,32 @@
-def main():
-    ...
+from pyfiglet import Figlet
+import sys
+import random
 
+figlet = Figlet()
+figlet_fonts = figlet.getFonts()
+
+def main():
+    str = input("Enter a text: ")
+    render_text(sys.argv, str)
+
+def render_random(str):
+    font = random.choice(figlet_fonts)
+    figlet.setFont(font=font)
+    print(figlet.renderText(str))
+
+def render_accurate(str, font):
+    figlet.setFont(font=font)
+    print(figlet.renderText(str))
+
+def render_text(argv, str):
+    if len(argv) == 1:
+        render_random(str)
+    elif len(argv) >= 3 and (argv[1] == "-f" or argv[1] == "--font"):
+        render_accurate(str, argv[2])
+    else:
+        sys.exit()
+
+        
+        
 if __name__ == "__main__":
     main()
