2dde86ae47e27a8d514f53bb2e59eeeb625d3076
Code179 <53229265+Code179@users.noreply.github.com>
2024-10-04 15:14:28 +0000

    Finished exercise

--- adieu.py
+++ adieu.py
@@ -1,5 +1,16 @@
+import inflect
+
 def main():
-    print("Hello, world")
+    p = inflect.engine()
+    names = []
+    while True:
+        try:
+            name = input("Name: ")
+            if name.strip(): names.append(name)
+        except EOFError:
+            print()
+            break
+    print(f"Adieu, adieu, to {p.join(names)}")
 
 if __name__ == "__main__":
     main()
