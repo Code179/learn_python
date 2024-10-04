eb1b166012e9bc0f0856c3037e2636391ab2b7bf
Code179 <53229265+Code179@users.noreply.github.com>
2024-10-04 15:41:25 +0000

    Finished exercise

--- game.py
+++ game.py
@@ -1,5 +1,26 @@
+import random
+
 def main():
-    print("Hello world")
+    number = 0
+    random_number = 0
+    guess = 0
+
+    while int(number) <= 0:
+        number = input("Enter a number: ")
+    random_number = random.randint(1, int(number))
+    print(random_number)
+    while int(guess) <= 0:
+        while int(guess) != random_number:
+            guess = input(f"Guess a number between 1 and {number}: ")
+            guess = int(guess)
+
+            if guess < random_number:
+                print("Too small!")
+            elif guess > random_number:
+                print("Too large!")
+            elif guess == random_number:
+                print("Congratulations!")
+                break
 
 if __name__ == "__main__":
     main()
