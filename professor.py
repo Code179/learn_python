0a10e06a42cd95d6fb0087dea277e6b608c69a1d
Code179 <53229265+Code179@users.noreply.github.com>
2024-10-06 23:42:41 +0000

    Everything done except score counting

--- professor.py
+++ professor.py
@@ -1,5 +1,67 @@
+import random
+
 def main():
-    print("Hello world")
+    level = get_level()
+    generate_integer(level)
+
+def get_level():
+    while True: 
+        level = input("Level: ")
+        try:
+            level = int(level)
+            if level in [1,2,3]:
+                return level
+            else:
+                print("EEE")
+        except ValueError:
+            print("EEE")
+
+def generate_integer(level):
+    for i in range(10):
+        if level == 1:
+            x = random.randint(0,9)
+            y = random.randint(0,9)
+            for i in range(3):
+                answer = input(f"{x} + {y} = ")
+                try:
+                    answer = int(answer)
+                    if answer == x + y:
+                        score += 1
+                        break
+                    elif answer != x + y and i == 3:
+                        print(f"{x} + {y} = {x + y}")
+                except ValueError:
+                    print("EEE")
+        elif level == 2:
+            x = random.randint(10,99)
+            y = random.randint(10,99)
+            for i in range(3):
+                answer = input(f"{x} + {y} = ")
+                try:
+                    answer = int(answer)
+                    if answer == x + y:
+                        break
+                    elif answer != x + y:
+                        print("EEE")
+                    if answer != x + y and i == 2:
+                        print(f"{x} + {y} = {x + y}")
+                except ValueError:
+                    print("EEE")
+                    if answer != x + y and i == 2:
+                        print(f"{x} + {y} = {x + y}")
+        elif level == 3:
+            x = random.randint(100,999)
+            y = random.randint(100,999)
+            for i in range(3):
+                answer = input(f"{x} + {y} = ")
+                try:
+                    answer = int(answer)
+                    if answer == x + y:
+                        break
+                    elif answer != x + y and i == 3:
+                        print(f"{x} + {y} = {x + y}")
+                except ValueError:
+                    print("EEE")
 
 if __name__ == "__main__":
     main()
