4e64a54b3d4e17761faf6e65fcb01f8463f285ac
Code179 <futureEntrepreneur3@gmail.com>
2024-10-14 08:21:41 +0000

    Added functionality to only count lines of code

--- lines.py
+++ lines.py
@@ -1,5 +1,16 @@
+import sys
+
 def main():
-  print("Hello, World")
-  
+  filename = sys.argv[1]
+  lines_of_code = 0
+  with open(filename) as file:
+    lines = file.readlines()
+    for line in lines:
+      line = line.strip()
+      if line != "\n":
+        if not line.startswith("#"):
+          lines_of_code +=1
+    print(lines_of_code)
+
 if __name__ == "__main__":
   main()
