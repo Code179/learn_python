6a0e9dbb53370e178815f14fa74eb994aa1b7346
Code179 <futureEntrepreneur3@gmail.com>
2024-10-26 10:35:58 +0000

    Adjusted handle command error function using os.path.splitext()

--- shirt.py
+++ shirt.py
@@ -1,17 +1,22 @@
 import sys
+import os
 
 def main():
     print("Hello, World")
     handle_cmd_args(sys.argv)
 
 def handle_cmd_args(args):
+  _, input_image_ext = os.path.splitext(args[1])
+  _, output_image_ext = os.path.splitext(args[2])
   try:
     if len(args) > 3:
       raise sys.exit("Too many arguments")
     elif len(args) < 3:
       raise sys.exit("Too few arguments")
-    elif len(args) == 3 and (not args[1].endswith((".jpg", ".png")) or not args[2].endswith((".jpg", ".png"))):
+    elif not input_image_ext.lower().endswith((".jpg", ".jpeg", ".png")) or not output_image_ext.lower().endswith((".jpg", ".jpeg", ".png")):
       raise sys.exit("Not a JPG or PNG File")
+    elif output_image_ext != input_image_ext:
+      raise sys.exit("Output ext different than input ext") 
     else:
       return args[1], args[2]
   except SystemExit as e:
