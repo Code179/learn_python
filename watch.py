a563fc154e3e725cbc849d81a1cc40365b6cdc45
Code179 <futureEntrepreneur3@gmail.com>
2024-10-29 14:29:59 +0100

    Finished exercise

--- watch.py
+++ watch.py
@@ -1,13 +1,20 @@
-import sys
 import re
 
 def main():
-    print("Hello, World!")
+    html = input("HTML: ")
+    print(parse(html))
 
-def parse(url):
-    ...
-
-
+def parse(html):
+    not_src = ".*?"
+    https = "(https://)"
+    www = r"www\."
+    pattern = rf"{not_src}src=\"{https}{www}(.*?)\"{not_src}"
+    if matches := re.search(pattern, html):
+        youtube_url = matches.group(2)
+        https = matches.group(1)
+        _,_,last = youtube_url.split("/")
+        new_youtube_url = f"{https}youtu.be/{last}"
+        return new_youtube_url
 
 if __name__ == "__main__":
     main()
