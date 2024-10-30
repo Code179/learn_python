bed65f4ee1ded1236a10e28a776af6ed26caa042
Code179 <futureEntrepreneur3@gmail.com>
2024-10-30 21:42:21 +0100

    Added TO-DO comments

--- working.py
+++ working.py
@@ -1,5 +1,12 @@
 import re
 
+# TO-DO: Convert a string like '9:00 AM to 5:00 PM' to '09:00 to 17:00'  
+# TO-DO: Convert a string like '9 AM to 5 PM' to '09:00 to 17:00'
+# TO-DO: Convert a string like '9:00 AM to 5 PM' to '09:00 to 17:00'
+# TO-DO: Convert a string like '9 AM to 5:00 PM' to '09:00 to 17:00'
+# TO-DO: Convert a string like '5:00 PM to 9:00 AM' to '09:00 to 17:00'
+# TO-DO: Raise ValueError if input is not in correct format like 13:00 PM or 00:00 PM
+
 def main():
     time = input("Hours: ")
     time_converted = convert(time)
