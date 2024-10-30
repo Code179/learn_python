fad61be426c3777f135fb386f91ee5be0e301aa8
Code179 <futureEntrepreneur3@gmail.com>
2024-10-30 21:33:28 +0100

    Setup exercise boilerplate

--- working.py
+++ working.py
@@ -1,5 +1,12 @@
+import re
+
 def main():
-    print("Hello, World")
+    time = input("Hours: ")
+    time_converted = convert(time)
+    print(time_converted)
+
+def convert(time):
+    return time
 
 if __name__ == "__main__":
     main()
