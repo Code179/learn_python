57a21b9fc540728482635480bfb2e6be89ebc2c1
Code179 <futureEntrepreneur3@gmail.com>
2024-11-01 19:41:40 +0000

    Finished exercise and added pycache folder to gitignore

---
+++ test_um.py
@@ -0,0 +1,12 @@
+from um import count
+
+def test_word_before():
+    assert count("Hello, um, World") == 1
+    assert count("Hello, um, Yummy World") == 1
+    assert count("Jesus asdhum") == 0
+
+def test_word_after():
+    assert count("Jesus umadhs") == 0
+
+def test_umm():
+    assert count("umm umm ummmmmmm") == 3
