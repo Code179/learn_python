217500a863b187ff6150947a4aef7fe96a591377
Code179 <futureEntrepreneur3@gmail.com>
2024-11-01 19:44:20 +0000

    setup boilerplate

--- test_um.py
+++
@@ -1,9 +0,0 @@
-from um import count
-
-def test_word_before():
-    assert count("Hello, um, World") == 1
-    assert count("Hello, um, Yummy World") == 1
-    assert count("Jesus asdhum") == 0
-
-def test_word_after():
-    assert count("Jesus umadhs") == 0
